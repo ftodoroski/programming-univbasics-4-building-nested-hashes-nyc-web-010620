6f0ea607ae7d4e234c0478f59c746aa308f14018
Filip Todoroski <ftodoroski@ymail.com>
2019-11-16 00:07:25 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,8 +20,8 @@ def monopoly_with_third_tier
 			:rent_in_dollars => {
 				:one_piece_owned => 25,
 				:two_pieces_owned => 50,
-				:two_pieces_owned that points to a fee of 50,
-				:two_pieces_owned that points to a fee of 50
+				:three_pieces_owned => 100,
+				:four_pieces_owned => 200
 			}
 		}
 
